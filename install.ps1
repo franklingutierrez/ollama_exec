8d1fa43fde2d7d443a25293a02a6055516da362b
Franklin Gutierrez <fgutierrezarizaca@gmail.com>
2024-10-13 23:29:19 -0500

    update

--- install.ps1
+++ install.ps1
@@ -2,6 +2,7 @@
 $pythonInstalled = Get-Command python -ErrorAction SilentlyContinue
 if (-not $pythonInstalled) {
     Write-Host "Python no está instalado. Por favor, instala Python antes de continuar."
+    Read-Host "Presiona Enter para salir"
     exit
 }
 
@@ -9,6 +10,7 @@ if (-not $pythonInstalled) {
 $gitInstalled = Get-Command git -ErrorAction SilentlyContinue
 if (-not $gitInstalled) {
     Write-Host "Git no está instalado. Por favor, instala Git antes de continuar."
+    Read-Host "Presiona Enter para salir"
     exit
 }
 
@@ -25,3 +27,6 @@ git checkout test
 
 # Ejecutar ollama.py
 python ollama.py
+
+# Pausa para mantener la ventana abierta
+Read-Host "Presiona Enter para salir"
